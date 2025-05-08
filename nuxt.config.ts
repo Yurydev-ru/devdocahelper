920a31970eeec3a00a5a7329643ef9ae0c641440
Yuryd <yurydevru@gmail.com>
2025-05-08 04:33:45 +0200

    Add commit message for changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   compatibilityDate: '2025-06-05',
   builder: 'vite',
   modules: ['@nuxtjs/tailwindcss'],

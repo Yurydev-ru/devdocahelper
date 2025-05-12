024eae9988b13cbbe45a48cd667495b2626557a3
Yuryd <yurydevru@gmail.com>
2025-05-13 01:07:40 +0200

    Added dark theme

---
+++ components/ui/ToggleTheme.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  const colorMode = useColorMode();
+  const toggleTheme = () => {
+    colorMode.value = colorMode.value === 'light' ? 'dark' : 'light';
+  }
+
+
+</script>
+
+<template>
+  <button @click="toggleTheme" class="btn btn-primary">
+    {{ colorMode.value === 'light' ? 'Dark' : 'Light' }}
+  </button>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
